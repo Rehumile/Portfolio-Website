e7e0b9a8b4ab956b3b9d7b336de4f3ccc2959e03
Rehumile <sekotorehumile1@gmail.com>
2023-08-23 21:05:50 +0200

    Worked on adding code into nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-
+import React from 'react'
 import './App.css'
+import Header from './components/Header/Header'
+
 
 function App() {
 
   return (
     <>
-      <h1>Portfolio website</h1>
+      <Header/>
     </>
   )
 }
