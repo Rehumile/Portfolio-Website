8f5ce4629fcf8774f6bdbde93ba472b3492df51f
Rehumile <sekotorehumile1@gmail.com>
2023-10-23 20:13:20 +0200

    Added code for qualifications section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header/Header'
 import Home from './components/Home/Home'
 import About from './components/About/About'
 import Skills from './components/Skills/Skills'
+import Qualifications from './components/Qualifications/Qualifications'
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Home/>
         <About />
         <Skills/>
+        <Qualifications/>
       </main>
     </>
   )
